adf3067bd90fab55561eb4a9001bc4fb7ddfdbb1
andressito09561 <andressito09561@gmail.com>
2025-01-08 09:18:45 +0100

    Add files via upload

---
+++ src/javainterfaz/gestionAdmin.java
@@ -0,0 +1,82 @@
+package javainterfaz;
+
+import java.awt.EventQueue;
+
+import javax.swing.JFrame;
+import javax.swing.JPanel;
+import javax.swing.border.EmptyBorder;
+import java.awt.BorderLayout;
+import javax.swing.JLabel;
+import javax.swing.JButton;
+import java.awt.FlowLayout;
+import java.awt.event.ActionListener;
+import java.awt.event.ActionEvent;
+
+public class gestionAdmin extends JFrame {
+
+	private static final long serialVersionUID = 1L;
+	private JPanel contentPane;
+
+	/**
+	 * Launch the application.
+	 */
+	public static void main(String[] args) {
+		EventQueue.invokeLater(new Runnable() {
+			public void run() {
+				try {
+					gestionAdmin frame = new gestionAdmin();
+					frame.setVisible(true);
+				} catch (Exception e) {
+					e.printStackTrace();
+				}
+			}
+		});
+	}
+
+	/**
+	 * Create the frame.
+	 */
+	public gestionAdmin() {
+		setDefaultCloseOperation(JFrame.EXIT_ON_CLOSE);
+		setBounds(100, 100, 450, 300);
+		contentPane = new JPanel();
+		contentPane.setBorder(new EmptyBorder(5, 5, 5, 5));
+
+		setContentPane(contentPane);
+		contentPane.setLayout(new BorderLayout(0, 0));
+		
+		JLabel lblLogo = new JLabel("LOGO");
+		contentPane.add(lblLogo, BorderLayout.NORTH);
+		
+		JPanel panel = new JPanel();
+		contentPane.add(panel, BorderLayout.CENTER);
+		panel.setLayout(new BorderLayout(0, 0));
+		
+		JPanel panel_1 = new JPanel();
+		panel.add(panel_1, BorderLayout.NORTH);
+		
+		JButton btnGestEq = new JButton("Gestionar equipos");
+		btnGestEq.addActionListener(new ActionListener() {
+			public void actionPerformed(ActionEvent e) {
+			}
+		});
+		panel_1.add(btnGestEq);
+		
+		JButton btnGestRes = new JButton("Gestionar resultados");
+		panel_1.add(btnGestRes);
+		
+		JPanel panel_2 = new JPanel();
+		panel.add(panel_2, BorderLayout.CENTER);
+		panel_2.setLayout(new FlowLayout(FlowLayout.CENTER, 5, 5));
+		
+		JButton btnGestJug = new JButton("Gestionar jugadores");
+		panel_2.add(btnGestJug);
+		
+		JButton btnGestTemp = new JButton("Gestionar temporadas");
+		panel_2.add(btnGestTemp);
+		
+		JButton btnCerrar = new JButton("Cerrar sesión");
+		contentPane.add(btnCerrar, BorderLayout.SOUTH);
+	}
+
+}
