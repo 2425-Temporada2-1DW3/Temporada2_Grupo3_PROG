402eae3d28296c9ad2dc43753d9d995ebae05713
andressito99 <andressito09561@gmail.com>
2025-01-30 22:04:20 +0100

    edit

--- src/ventanas/JugadoresWindow.java
+++ src/ventanas/JugadoresWindow.java
@@ -42,7 +42,7 @@ public class JugadoresWindow extends JFrame {
     public JugadoresWindow() {
         setTitle("Gestion Jugadores - Txurdi Liga");
         setDefaultCloseOperation(JFrame.EXIT_ON_CLOSE);
-        setBounds(100, 100, 450, 300);
+        setBounds(100, 100, 730, 300);
         contentPane = new JPanel();
         contentPane.setBorder(new EmptyBorder(5, 5, 5, 5));
         setContentPane(contentPane);
